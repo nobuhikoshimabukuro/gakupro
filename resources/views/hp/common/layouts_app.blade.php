0666408f834d3673a57c3cc44e8dd6043edf5815
shimabukuronobuhiko <shimabukuronobuhiko@gmail.com>
2023-01-17 09:18:25 +0900

    修正

--- resources/views/hp/common/layouts_app.blade.php
+++ resources/views/hp/common/layouts_app.blade.php
@@ -8,6 +8,7 @@
     <link href="{{ asset('css/hp/common.css') }}" rel="stylesheet">      
     <link href="{{ asset('css/hp/footer.css') }}" rel="stylesheet">
     
+    <link href="{{ asset('css/hp/style.css') }}" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">  {{-- CSRFトークン --}}
     @yield('pagehead')
     <title>@yield('title')</title>
