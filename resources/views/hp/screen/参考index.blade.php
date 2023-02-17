77362d06835029631e73c4feb21b4e6c9d6f4105
PC-SHIMABUKURO\Developer <shimabukuronobuhiko@gmail.com>
2023-02-18 01:24:54 +0900

    no message

---
+++ resources/views/hp/screen/参考index.blade.php
@@ -0,0 +1,744 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>求人サイト（企業・ビジネスサイト）向け 無料ホームページテンプレート tp_biz57_job</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ここにサイト説明を入れます">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<div id="container">
+
+<header>
+<h1 id="logo"><a href="index.html"><img src="images/logo.png" alt="SAMPLE COMPANY"></a></h1>
+<div id="tel">0120-000-0000<br><span class="small">受付9:00〜18:00/日祝休</span></div>
+</header>
+
+<div id="menubar">
+
+<!--メインメニュー-->
+<nav id="mainmenu">
+<ul>
+<li><a href="index.html">ホーム<span>Home</span></a></li>
+<li><a href="list_base.html">求人一覧<span>Jobs List</span></a>
+	<ul>
+	<li><a href="list.html">飲食店の求人</a></li>
+	<li><a href="list.html">営業の求人</a></li>
+	<li><a href="list.html">接客・販売の求人</a></li>
+	<li><a href="list.html">事務の求人</a></li>
+	</ul>
+</li>
+<li><a href="info.html">掲載のご案内<span>Information</span></a></li>
+<li><a href="faq.html">よく頂く質問<span>Faq</span></a></li>
+<li><a href="contact.html">お問い合わせ<span>Contact</span></a></li>
+</ul>
+</nav>
+
+<div class="sh">
+
+<p>※小さな端末のメニュー開閉時に追加したいブロックがあればここ（shボックスの中）に入れて下さい。</p>
+<ul class="submenu">
+<li><a href="#">小さな端末用メニュー</a></li>
+<li><a href="#">小さな端末用メニュー</a></li>
+<li><a href="#">小さな端末用メニュー</a></li>
+<li><a href="#">小さな端末用メニュー</a></li>
+<li><a href="#">小さな端末用メニュー</a></li>
+</ul>
+
+</div>
+<!--/.sh-->
+
+</div>
+<!--/#menubar-->
+
+<!--スライドショー（slick）-->
+<div class="mainimg">
+<div><img src="images/1.jpg" alt=""></div>
+<div><img src="images/2.jpg" alt=""></div>
+<div><img src="images/3.jpg" alt=""></div>
+</div>
+
+<div id="contents">
+
+<main>
+
+<section>
+
+<h2>テンプレートのご利用前に必ずお読み下さい</h2>
+
+<h3>利用規約のご案内</h3>
+<p>このテンプレートは、<a href="https://template-party.com/">Template Party</a>にて無料配布している『求人サイト（企業・ビジネスサイト）向け 無料ホームページテンプレート tp_biz57_job』です。必ずダウンロード先のサイトの<a href="https://template-party.com/read.html">利用規約</a>をご一読の上でご利用下さい。</p>
+<p><strong class="color-theme">HP最下部の著作表示『Web Design:Template-Party』は無断で削除しないで下さい。</strong><br>
+わざと見えなく加工する事も禁止です。</p>
+<p><strong class="color-theme">下部の著作を外したい場合は</strong><br>
+<a href="https://template-party.com/">Template-Party</a>の<a href="https://template-party.com/member.html">ライセンス契約</a>を行う事でHP下部の著作を外す事ができます。</p>
+
+<h3>トップページのスライドショー</h3>
+<p>トップページのスライドショー写真は、配布元サイト（写真ACさん）の規約により梱包できませんが、配布元サイト(<a href="https://www.photo-ac.com/#a_aid=60e590da89d1f&amp;a_bid=35660fdf" target="_blank">写真AC</a><img style="border:0" src="https://acworks.postaffiliatepro.com/scripts/g5iueh?a_aid=60e590da89d1f&amp;a_bid=35660fdf" width="1" height="1" alt="" />)さんから無料ダウンロードができます。<br>
+ご利用の際はACさんの規約を読んだ上で直接ダウンロードして下さい。</p>
+<ul>
+<li>
+<a href="https://www.photo-ac.com/main/detail/24511324#a_aid=60e590da89d1f&amp;a_bid=35660fdf" target="_blank">１枚目写真のDL先（※リンク先はACさんです）</a><img style="border:0" src="https://acworks.postaffiliatepro.com/scripts/g5iueh?a_aid=60e590da89d1f&amp;a_bid=35660fdf" width="1" height="1" alt="" /></li>
+<li><a href="https://www.photo-ac.com/main/detail/22563540#a_aid=60e590da89d1f&amp;a_bid=35660fdf" target="_blank">２枚目写真のDL先（※リンク先はACさんです）</a><img style="border:0" src="https://acworks.postaffiliatepro.com/scripts/g5iueh?a_aid=60e590da89d1f&amp;a_bid=35660fdf" width="1" height="1" alt="" /></li>
+<li><a href="https://www.photo-ac.com/main/detail/23130400#a_aid=60e590da89d1f&amp;a_bid=35660fdf" target="_blank">３枚目写真のDL先（※リンク先はACさんです）</a><img style="border:0" src="https://acworks.postaffiliatepro.com/scripts/g5iueh?a_aid=60e590da89d1f&amp;a_bid=35660fdf" width="1" height="1" alt="" /></li>
+</ul>
+
+<h3 id="page">当テンプレートにはお問い合わせフォーム（自動フォーム：試用版）がセットされています</h3>
+<p><a href="contact.html">contact.html</a>と同じ３項目のお問い合わせフォームを簡単に使えるようにセットしています。</p>
+<p><strong class="color-theme">自動フォームを使う場合（※編集に入る前にご確認下さい）</strong><br>
+あなたのメールアドレス設定と、簡単な編集だけで使えます。<a href="https://template-party.com/file/formgen_manual_set2.html" target="_blank">詳しくはこちらのマニュアルをご覧下さい。</a></p>
+<p><strong class="color-theme">自動フォームを使わない場合</strong><br>
+テンプレートに梱包されている「form.html」「confirm.html」「finish.html」の3枚のファイルを削除して下さい。</p>
+
+<h3>当テンプレートの詳しい使い方は</h3>
+<p><a href="company.html">こちらをご覧下さい。</a></p>
+
+</section>
+
+<section>
+<h2>求人向けCMS販売中<span>CMS</span></h2>
+<p>求人サイト向けプログラムを販売中です。<br>
+求人一覧ページ以外に、各詳細ページや専用お問い合わせフォームも出力できます。<br>
+<a href="https://template-party.com/cms_db/siyou_job.html">無料で試せる『試用版』公開中。</a></p>
+<p><a href="https://template-party.com/base_cms.html#db"><img src="https://template-party.com/cms_db/images/img01_job.jpg" alt=""></a></p>
+</section>
+
+<section>
+
+<h2>求人一覧（listタイプ）</h2>
+
+<div class="list-container">
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<span class="icon waku">正社員</span>
+	<span class="icon waku">アルバイト</span>
+	<span class="icon newicon">NEW</span>
+	<span class="icon upicon">UP</span>
+	<span class="icon option1">人気</span>
+	<span class="icon option2">急募</span>
+	<span class="icon option3">終了</span>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		<dt>タイトル</dt>
+		<dd>内容。</dd>
+		</dl>
+	</div>
+	<p class="btn arrow"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn arrow"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<div class="list">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+</div>
+
+<div class="list">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+</div>
+
+</div>
+<!--/.list-container-->
+
+</section>
+
+<section>
+
+<h2>求人一覧（list2タイプ）</h2>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4>
+	<a href="item.html">WEBデザイナー募集！未経験歓迎！</a>
+	<span class="icon waku">正社員</span>
+	<span class="icon waku">アルバイト</span>
+	<span class="icon newicon">NEW</span>
+	<span class="icon upicon">UP</span>
+	<span class="icon option1">人気</span>
+	<span class="icon option2">急募</span>
+	<span class="icon option3">終了</span>
+	</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4>
+	<a href="item.html">WEBデザイナー募集！未経験歓迎！</a>
+	<span class="icon waku">正社員</span>
+	<span class="icon waku">アルバイト</span>
+	<span class="icon newicon">NEW</span>
+	<span class="icon upicon">UP</span>
+	<span class="icon option1">人気</span>
+	<span class="icon option2">急募</span>
+	<span class="icon option3">終了</span>
+	</h4>
+	<p>ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。</p>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	<p>ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。</p>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4>
+	<a href="item.html">WEBデザイナー募集！未経験歓迎！</a>
+	<span class="icon waku">正社員</span>
+	<span class="icon waku">アルバイト</span>
+	<span class="icon newicon">NEW</span>
+	<span class="icon upicon">UP</span>
+	<span class="icon option1">人気</span>
+	<span class="icon option2">急募</span>
+	<span class="icon option3">終了</span>
+	</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4>
+	<a href="item.html">WEBデザイナー募集！未経験歓迎！</a>
+	<span class="icon waku">正社員</span>
+	<span class="icon waku">アルバイト</span>
+	<span class="icon newicon">NEW</span>
+	<span class="icon upicon">UP</span>
+	<span class="icon option1">人気</span>
+	<span class="icon option2">急募</span>
+	<span class="icon option3">終了</span>
+	</h4>
+	<p>ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。</p>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	<p>ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。ここに説明を入れます。サンプルテキスト。</p>
+	</div>
+</div>
+
+</section>
+
+<section>
+
+<h2>お知らせ</h2>
+
+<dl id="new">
+<dt>2022/10/07<span>その他</span></dt>
+<dd>Windows版ChromeやFirefoxでドロップダウンを使った場合、移動先ページでドロップダウンが残ってしまう不具合があったのを改善。<br>
+<a href="https://template-party.com/update/20221006_biz57.html">詳細はこちらをご覧下さい。</a></dd>
+<dt>2022/08/22<span>その他</span></dt>
+<dd>tp_biz57_job配布開始。</dd>
+<dt>20XX/00/00<span class="icon-bg1">求人</span></dt>
+<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+<dt>20XX/00/00<span class="icon-bg1">求人</span></dt>
+<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+<dt>20XX/00/00</dt>
+<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+<dt>20XX/00/00<span>その他</span></dt>
+<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+</dl>
+
+</section>
+
+</main>
+
+<div id="sub">
+
+<nav class="box1">
+<h2>メニュータイトル(subブロック)</h2>
+<ul class="submenu">
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+</nav>
+
+<nav>
+<h2>メニュータイトル(subブロック)</h2>
+<ul class="submenu">
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+</nav>
+
+<h2>今月の注目(subブロック)</h2>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4>WEBデザイナー募集！未経験歓迎！</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4>WEBデザイナー募集！未経験歓迎！</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4>WEBデザイナー募集！未経験歓迎！</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<h2>今月の注目(subブロック)</h2>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+</div>
+<!--/#sub-->
+
+<div id="side">
+
+<nav class="box1">
+<h2>メニュータイトル(sideブロック)</h2>
+<ul class="submenu">
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+</nav>
+
+<nav>
+<h2>メニュータイトル(sideブロック)</h2>
+<ul class="submenu">
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+<li><a href="#">メニューサンプル</a></li>
+</ul>
+</nav>
+
+<h2>今月の注目(sideブロック)</h2>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！未経験歓迎！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<div class="list2">
+	<div>
+	<h4><a href="item.html">WEBデザイナー募集！</a></h4>
+	</div>
+</div>
+
+<h2>今月の注目(sideブロック)</h2>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4>WEBデザイナー募集！未経験歓迎！</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4>WEBデザイナー募集！未経験歓迎！</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn"><a href="item.html">詳細を見る</a></p>
+</div>
+
+<div class="list">
+	<div>
+	<figure><a href="item.html"><img src="images/sample1.jpg" alt=""></a></figure>
+	<h4>WEBデザイナー募集！未経験歓迎！</h4>
+		<dl class="line">
+		<dt>求人ナンバー</dt>
+		<dd>No.XXX</dd>
+		<dt>企業名</dt>
+		<dd>サンプルカンパニー</dd>
+		<dt>給与・時給</dt>
+		<dd>時給1,100円〜　研修期間：時給800円</dd>
+		<dt>勤務地</dt>
+		<dd>千葉市中央区</dd>
+		<dt>勤務時間</dt>
+		<dd>19:00〜　1日3時間程度</dd>
+		</dl>
+	</div>
+	<p class="btn"><a href="item.html">詳細を見る</a></p>
+</div>
+</div>
+<!--/#side-->
+
+</div>
+<!--/#contents-->
+
+<div id="footermenu">
+<ul>
+<li class="title">メニュー</li>
+<li><a href="index.html">ホーム</a></li>
+<li><a href="company.html">会社概要</a></li>
+</ul>
+<ul>
+<li class="title">メニュー見出し</li>
+<li><a href="#">サンプルメニューサンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+</ul>
+<ul>
+<li class="title">メニュー見出し</li>
+<li><a href="#">サンプルメニューサンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+</ul>
+<ul>
+<li class="title">メニュー見出し</li>
+<li><a href="#">サンプルメニューサンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+<li><a href="#">サンプルメニュー</a></li>
+</ul>
+</div>
+<!--/#footermenu-->
+
+<footer>
+<small>Copyright&copy; <a href="index.html">SAMPLE COMPANY</a> All Rights Reserved.</small>
+<span class="pr"><a href="https://template-party.com/" target="_blank">《Web Design:Template-Party》</a></span>
+</footer>
+
+</div>
+<!--/#container-->
+
+<!--jQueryの読み込み-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!--biz57用のスクリプト-->
+<script src="js/main.js"></script>
+
+<!--ハンバーガーボタン（開閉操作のボタン）-->
+<div id="menubar_hdr"></div>
+
+<!--ページの上部へ戻るボタン-->
+<div class="pagetop"><a href="#"><i class="fas fa-angle-double-up"></i></a></div>
+
+<!--スライドショー（slick）-->
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="js/slick.js"></script>
+
+</body>
+</html>
